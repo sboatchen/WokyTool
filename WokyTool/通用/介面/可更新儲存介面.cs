be17136147ab45ba29d09a73f34967518566552e
sboat <sboatchen@gmail.com>
2018-06-25 14:55:37 +0800

    [ADD]missing commit

---
+++ WokyTool/通用/介面/可更新儲存介面.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WokyTool.通用
+{
+    public interface 可更新儲存介面
+    {
+        void 更新資料();
+        void 儲存修改();
+    }
+}
