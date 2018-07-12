57156ffe30b47898f46ee125e58a21d4c74c10d8
sboat <sboatchen@gmail.com>
2018-07-12 22:24:58 +0800

    [ADD]商品大小類 視窗

---
+++ WokyTool/商品/視窗/商品大類總覽視窗.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WokyTool.通用;
+
+namespace WokyTool.商品
+{
+    public partial class 商品大類總覽視窗 : 總覽視窗
+    {
+        public 商品大類總覽視窗()
+        {
+            InitializeComponent();
+
+            this.初始化<商品大類資料>(this.商品大類資料BindingSource, 商品大類資料管理器.獨體);
+        }
+    }
+}
---
+++ WokyTool/商品/視窗/商品小類總覽視窗.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WokyTool.通用;
+
+namespace WokyTool.商品
+{
+    public partial class 商品小類總覽視窗 : 總覽視窗
+    {
+        public 商品小類總覽視窗()
+        {
+            InitializeComponent();
+
+            this.初始化<商品小類資料>(this.商品小類資料BindingSource, 商品小類資料管理器.獨體);
+        }
+    }
+}
