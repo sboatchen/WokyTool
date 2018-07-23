5366c56fb5e38480208d9e148c2e5a4d208025dc
sboat <sboatchen@gmail.com>
2018-07-24 04:33:38 +0800

    TEMP

--- WokyTool/月結帳/控制/月結帳資料管理器.cs
+++ WokyTool/月結帳/控制/月結帳資料管理器.cs
@@ -1,25 +1,21 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 using WokyTool.Common;
-using WokyTool.DataMgr;
 using WokyTool.通用;
 
 namespace WokyTool.月結帳
 {
     public class 月結帳資料管理器 : 資料管理器<月結帳資料>
     {
-        public override string 檔案路徑 
+        public override string 檔案路徑
         {
-            get 
-            { 
-                return "進度/月結帳.json"; 
-            } 
+            get
+            {
+                return "進度/月結帳.json";
+            }
         }
 
         public override 月結帳資料 空白資料
@@ -32,18 +28,18 @@ namespace WokyTool.月結帳
 
         public override 月結帳資料 錯誤資料
         {
-            get 
+            get
             {
-                return 月結帳資料.ERROR; 
-            } 
+                return 月結帳資料.ERROR;
+            }
         }
 
-        public override 列舉.編碼類型 編碼類型 
-        { 
-            get 
-            { 
-                return 列舉.編碼類型.月結帳; 
-            } 
+        public override 列舉.編碼類型 編碼類型
+        {
+            get
+            {
+                return 列舉.編碼類型.月結帳;
+            }
         }
 
         // 獨體
--- WokyTool/月結帳/資料/月結帳資料.cs
+++ WokyTool/月結帳/資料/月結帳資料.cs
@@ -5,8 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WokyTool.Common;
-using WokyTool.Data;
-using WokyTool.DataMgr;
+using WokyTool.公司;
+using WokyTool.客戶;
+using WokyTool.商品;
 using WokyTool.通用;
 
 namespace WokyTool.月結帳
@@ -17,52 +18,71 @@ namespace WokyTool.月結帳
         [JsonProperty]
         public override int 編號 { get; set; }
 
-        public 公司資料 公司;
         [JsonProperty]
         public int 公司編號
         {
             get
             {
-                if (公司 == null)
-                    return 常數.T錯誤資料編碼;
                 return 公司.編號;
             }
             set
             {
-                公司 = 公司管理器.Instance.Get(value);
+                _公司 = 公司資料管理器.獨體.Get(value);
             }
         }
 
-        public String 公司名稱 
-        {
-            get { return 公司.名稱; }
-        }
-
-        public 廠商資料 廠商;
-        [JsonProperty]
-        public int 廠商編號
+        protected 公司資料 _公司;
+        public 公司資料 公司
         {
             get
             {
-                if (廠商 == null)
-                    return 常數.T錯誤資料編碼;
-                return 廠商.編號;
+                if (_公司 == null)
+                    _公司 = 公司資料.NULL;
+                else if (公司資料管理器.獨體.唯讀BList.Contains(_公司) == false)
+                    _公司 = 公司資料.ERROR;
+
+                return _公司;
             }
             set
             {
-                廠商 = 廠商管理器.Instance.Get(value);
+                _公司 = value;
             }
         }
 
-        public String 廠商名稱
+        [JsonProperty]
+        public int 客戶編號
         {
-            get { return 廠商.名稱; }
+            get
+            {
+                return 客戶.編號;
+            }
+            set
+            {
+                _客戶 = 客戶資料管理器.獨體.Get(value);
+            }
+        }
+
+        protected 客戶資料 _客戶;
+        public 客戶資料 客戶
+        {
+            get
+            {
+                if (_客戶 == null)
+                    _客戶 = 客戶資料.NULL;
+                else if (客戶資料管理器.獨體.唯讀BList.Contains(_客戶) == false)
+                    _客戶 = 客戶資料.ERROR;
+
+                return _客戶;
+            }
+            set
+            {
+                _客戶 = value;
+            }
         }
 
         [JsonProperty]
         public string 商品識別 { get; set; }
 
-        public 商品資料 商品 { get; set; }
         [JsonProperty]
         public int 商品編號
         {
@@ -72,13 +92,26 @@ namespace WokyTool.月結帳
             }
             set
             {
-                商品 = 商品管理器.Instance.Get(value);
+                _商品 = 商品資料管理器.獨體.Get(value);
             }
         }
 
-        public String 商品名稱
+        protected 商品資料 _商品;
+        public 商品資料 商品
         {
-            get { return 商品.名稱; }
+            get
+            {
+                if (_商品 == null)
+                    _商品 = 商品資料.NULL;
+                else if (商品資料管理器.獨體.唯讀BList.Contains(_商品) == false)
+                    _商品 = 商品資料.ERROR;
+
+                return _商品;
+            }
+            set
+            {
+                _商品 = value;
+            }
         }
 
         [JsonProperty]
@@ -90,20 +123,52 @@ namespace WokyTool.月結帳
         [JsonProperty]
         public decimal 含稅單價 { get; set; }
 
-        public decimal 總金額 
+        public decimal 總金額
         {
             get { return 數量 * 含稅單價; }
         }
 
+        public decimal 成本
+        {
+            get
+            {
+                return 商品.成本;
+            }
+        }
+
+        public decimal 利潤
+        {
+            get
+            {
+                return 含稅單價 - 成本;
+            }
+        }
+
+        public decimal 總利潤
+        {
+            get
+            {
+                return 利潤 * 數量;
+            }
+        }
+
         /********************************/
+
+        public 月結帳資料 Self
+        {
+            get { return this; }
+        }
 
         private static readonly 月結帳資料 _NULL = new 月結帳資料
         {
             編號 = 常數.T空白資料編碼,
+
             公司 = 公司資料.NULL,
-            廠商 = 廠商資料.NULL,
-            商品識別 = 字串.空,
+            客戶 = 客戶資料.NULL,
+
+            商品識別 = 字串.無,
             商品 = 商品資料.NULL,
+
             數量 = 0,
             單價 = 0,
             含稅單價 = 0,
@@ -119,10 +184,13 @@ namespace WokyTool.月結帳
         private static 月結帳資料 _ERROR = new 月結帳資料
         {
             編號 = 常數.T錯誤資料編碼,
+
             公司 = 公司資料.ERROR,
-            廠商 = 廠商資料.ERROR,
-            商品識別 = 字串.空,
+            客戶 = 客戶資料.ERROR,
+
+            商品識別 = 字串.錯誤,
             商品 = 商品資料.ERROR,
+
             數量 = 0,
             單價 = 0,
             含稅單價 = 0,
@@ -142,10 +210,13 @@ namespace WokyTool.月結帳
             月結帳資料 Data_ = new 月結帳資料
             {
                 編號 = this.編號,
+
                 公司 = this.公司,
-                廠商 = this.廠商,
+                客戶 = this.客戶,
+
                 商品識別 = this.商品識別,
                 商品 = this.商品,
+
                 數量 = this.數量,
                 單價 = this.單價,
                 含稅單價 = this.含稅單價,
@@ -157,10 +228,13 @@ namespace WokyTool.月結帳
         public override void 覆蓋(月結帳資料 Data_)
         {
             編號 = Data_.編號;
+
             公司 = Data_.公司;
-            廠商 = Data_.廠商;
+            客戶 = Data_.客戶;
+
             商品識別 = Data_.商品識別;
             商品 = Data_.商品;
+
             數量 = Data_.數量;
             單價 = Data_.單價;
             含稅單價 = Data_.含稅單價;
@@ -170,10 +244,13 @@ namespace WokyTool.月結帳
         {
             return
                 編號 == Data_.編號 &&
+
                 公司 == Data_.公司 &&
-                廠商 == Data_.廠商 &&
+                客戶 == Data_.客戶 &&
+
                 商品識別 == Data_.商品識別 &&
                 商品 == Data_.商品 &&
+
                 數量 == Data_.數量 &&
                 單價 == Data_.單價 &&
                 含稅單價 == Data_.含稅單價;
@@ -181,14 +258,15 @@ namespace WokyTool.月結帳
 
         public override void 檢查合法()
         {
-            if (公司編號 <= 常數.T新建資料編碼) //@@ 編號 <= 常數.T新建資料編碼 -> .編號是否合法() == false
-                throw new Exception("月結帳資料:公司不合法:" + 公司編號);
+            if (公司.編號是否合法() == false)
+                throw new Exception("月結帳資料:公司編號不合法:" + 公司編號);
 
-            if (廠商編號 <= 常數.T新建資料編碼)
-                throw new Exception("月結帳資料:廠商不合法:" + 廠商編號);
+            if (客戶.編號是否合法() == false)
+                throw new Exception("月結帳資料:客戶編號不合法:" + 客戶編號);
 
-            if (商品編號 <= 常數.T新建資料編碼)
+            if (商品.編號是否合法() == false)
                 throw new Exception("月結帳資料:商品不合法:" + 商品編號);
         }
     }
 }
+
