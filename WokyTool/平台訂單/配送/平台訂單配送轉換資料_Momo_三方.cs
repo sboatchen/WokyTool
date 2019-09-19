081bc9b2fc0b7a8b9c21f549b00a2307f73c0465
sboat <sboatchen@gmail.com>
2019-09-19 14:36:40 +0800

    [TEMP]

--- WokyTool/平台訂單/配送/平台訂單配送轉換資料_Momo_三方.cs
+++ WokyTool/平台訂單/配送/平台訂單配送轉換資料_Momo_三方.cs
@@ -41,22 +41,20 @@ namespace WokyTool.平台訂單
             {
                 if (_設定資料書 == null)
                 {
-                    // 讀出 發票
-                    // 寫入 物品組成
                     _設定資料書 = new Dictionary<三方讀出元件組, PDF字串寫入元件>();
                     _設定資料書.Add(new 三方讀出元件組
                     {
                         發票讀出元件 = new PDF字串讀出元件(new Rectangle(80, 515, 130, 525)),
                         配送單號讀出元件 = new PDF字串讀出元件(new Rectangle(75, 695, 150, 710)),
                         消費者讀出元件 = new PDF字串讀出元件(new Rectangle(70, 640, 280, 680))
-                    }, new PDF字串寫入元件(new Rectangle(5, 562, 295, 603), 常數.通用字體));    //465 ct.SetSimpleColumn(myText, 290, WritePosY_ + 20, 520, WritePosY_ - 100, 15, Element.ALIGN_TOP);
+                    }, new PDF字串寫入元件(new Rectangle(290, 430, 595, 475), 常數.通用字體));
 
                     _設定資料書.Add(new 三方讀出元件組
                     {
                         發票讀出元件 = new PDF字串讀出元件(new Rectangle(80, 80, 130, 90)),
                         配送單號讀出元件 = new PDF字串讀出元件(new Rectangle(75, 265, 100, 280)),
                         消費者讀出元件 = new PDF字串讀出元件(new Rectangle(70, 200, 280, 250))
-                    }, new PDF字串寫入元件(new Rectangle(5, 562, 295, 603), 常數.通用字體));    //30 ct.SetSimpleColumn(myText, 290, WritePosY_ + 20, 520, WritePosY_ - 100, 15, Element.ALIGN_TOP);
+                    }, new PDF字串寫入元件(new Rectangle(290, 0, 595, 42), 常數.通用字體));
                 }
 
                 return _設定資料書;
@@ -87,19 +85,19 @@ namespace WokyTool.平台訂單
                     return;
 
                 發票號碼_ = 發票號碼_.Substring(7);
-                Console.WriteLine("發票號碼 " + 發票號碼_);
+                //Console.WriteLine("發票號碼 " + 發票號碼_);
 
-                this.配送單號 = Pair_.Key.配送單號讀出元件.處理(PdfReader_, 頁索引_).Substring(5);
-                Console.WriteLine("配送單號 " + 配送單號);
+                string 配送單號_ = Pair_.Key.配送單號讀出元件.處理(PdfReader_, 頁索引_).Substring(5);
+                //Console.WriteLine("配送單號 " + 配送單號_);
 
                 // 取得消費者資料
                 string 消費者資料字串_ = Pair_.Key.消費者讀出元件.處理(PdfReader_, 頁索引_);
                 string[] 消費者資料_ = 消費者資料字串_.ToString().Split(消費者資料拆分字元組);
-                this.地址 = 消費者資料_[0];
-                this.電話 = 消費者資料_[1];
-                this.手機 = 消費者資料_[2];
-                this.姓名 = 消費者資料_[3];
-                Console.WriteLine("消費者:{0},{1},{2},{3}", this.姓名, this.地址, this.電話, this.手機);
+                string 地址_ = 消費者資料_[0];
+                string 電話_ = 消費者資料_[1];
+                string 手機_ = 消費者資料_[2];
+                string 姓名_ = 消費者資料_[3];
+                //Console.WriteLine("消費者:{0},{1},{2},{3}", 姓名_, 地址_, 電話_, 手機_);
 
                 var 符合資料列_ = 來源資料列.Where(Value => 發票號碼_.Equals(Value.發票號碼) && Value.處理狀態 == 列舉.訂單處理狀態.新增).ToArray();
                 if (符合資料列_.Length == 0)
@@ -111,23 +109,21 @@ namespace WokyTool.平台訂單
                 {
                     var 分組群_ =  符合資料列_.GroupBy(Value => Value.配送分組);
                     if(分組群_.Count() > 1)
-                        訊息管理器.獨體.警告("找到多個同發票號碼的組別，請注意: " + 名字_);
+                        訊息管理器.獨體.警告("找到多個同發票號碼的組別，請注意: " + 姓名_);
 
                     物品合併資料 物品合併資料_ = new 物品合併資料();
                     foreach (平台訂單新增資料 資料_ in 符合資料列_)
                     {
                         物品合併資料_.新增(資料_);
 
-                        姓名 = 名字_;
-                        電話 = 電話_;
-                        手機 = 手機_;
-                        地址 = 地址_;
-                        配送單號 = 配送單號_;
+                        資料_.BeginEdit();
+                        資料_.姓名 = 姓名_;
+                        資料_.電話 = 電話_;
+                        資料_.手機 = 手機_;
+                        資料_.地址 = 地址_;
+                        資料_.配送單號 = 配送單號_;
                         資料_.處理狀態 = 列舉.訂單處理狀態.配送;
-
-                        資料_.
                     }
-                        物品合併資料_.新增(資料_);
 
                     Pair_.Value.處理(PdfWriter_, 物品合併資料_.ToString());
                 }
@@ -136,24 +132,6 @@ namespace WokyTool.平台訂單
 
         public override void 測試(PdfReader PdfReader_, int 頁索引_, PdfWriter PdfWriter_)
         {
-            /*foreach (PDF拷貝元件 拷貝資料_ in 拷貝資料列)
-            {
-                拷貝資料_.處理(PdfReader_, 頁索引_, PdfWriter_);
-            }
-
-            //PDF定位元件 PDF定位元件_ = new PDF定位元件();
-            //PDF定位元件_.處理(PdfWriter_);
-
-            foreach (var Pair_ in 設定資料書)
-            {
-                string 配送單號_ = Pair_.Key.處理(PdfReader_, 頁索引_);
-                if (string.IsNullOrEmpty(配送單號_))
-                    return;
-
-                訊息管理器.獨體.訊息("讀出配送單號:" + 配送單號_);
-
-                Pair_.Value.處理(PdfWriter_, 字串.多字測試);
-            }*/
         }
     }
 }
